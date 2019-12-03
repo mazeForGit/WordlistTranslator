9916c97b2775078700a88892171090484a9ef4ea
Mathias Fischer <better.no.software@o2online.de>
2019-12-03 13:16:23 +0100

    update ci

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Stops the process if something fails
-set -xe
+# set -xe
 
 # All of the dependencies needed/fetched for your project.
 # This is what actually fixes the problem so that EB can find your dependencies. 
 # FOR EXAMPLE:
-go get "github.com/gin-gonic/gin"
+# go get "github.com/gin-gonic/gin"
 
 # create the application binary that eb uses
 GOOS=linux GOARCH=amd64 go build -o bin/application -ldflags="-s -w"
