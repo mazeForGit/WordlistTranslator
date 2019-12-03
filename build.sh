b609f50ed0d4eef5c448f70ac7b5855667a71d57
Mathias Fischer <better.no.software@o2online.de>
2019-12-03 15:07:57 +0100

    update ci

--- build.sh
+++ build.sh
@@ -7,7 +7,13 @@ echo "starting build.sh"
 # All of the dependencies needed/fetched for your project.
 # This is what actually fixes the problem so that EB can find your dependencies. 
 # FOR EXAMPLE:
+
+echo "get dependencies"
 # go get "github.com/gin-gonic/gin"
 
 # create the application binary that eb uses
 GOOS=linux GOARCH=amd64 go build -o bin/application -ldflags="-s -w"
+
+chmod +x bin/application
+
+echo "build successful"
